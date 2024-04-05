cab94995456aa4565c92737f21d2ac8ced18ff9e
harmeilia14 <harmeilia14@gmail.com>
2024-04-05 17:43:35 +0700

    first commit

---
+++ pertemuan1/src/components/TheWelcome.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <h2><i>Pendidikan</i></h2>
+        SDN 79 Pekanbaru 2010 - 15 <br>
+        SMPN 34 Pekanbaru 2016 - 2018 <br>
+        SMK Labor Pekanbaru 2019 - 2021 <br>
+        Universitas Islam Riau 2022 - Sekarang
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <h2><i>Skill</i></h2>
+      Power Point <br>
+      PHP <br>
+      C++ <br>
+      Database
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <h2><i>Pengalaman</i></h2>
+      Melakukan Praktik Kerja lapangan di KLT Badan Standarisasi Nasional
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <h2><i>Kontak</i></h2>
+      <a href="https://www.instagram.com/harmeiliareski/" target="_blank" rel="noopener">Instagram</a> <br>
+      <a href="https://wa.me/qr/V7BOA7SLLNZZC1" target="_blank" rel="noopener">WhatsApp</a>
+  </WelcomeItem>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <div class="row">
+              <div class="col-lg-6">
+                <img  src="/public/assets/contoh.jpg" width="200px" height="250px" />
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Nama</strong> <span>Harmeilia Reski Rahmayani</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tempat, Tanggal lahir</strong> <span>Pekanbaru, 14 Mei 2004</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Jenis Kelamin</strong> <span>Perempuan</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Alamat</strong> <span>Jl. Muhajirin, Perum. Purna Griya Mas </span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>No Telp</strong> <span>0895 8012 09010</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>email</strong> <span>harmeilia14@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Pendidikan</strong> <span>Mahasiswa Universitas Islam Riau</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+  </div> 
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+img {
+  border-radius: 50%;
+}
+
+
+
+</style>
